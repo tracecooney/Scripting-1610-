548fdfc05b3c29103ec8525ee6fd3c0094fcd551
trace cooney <trace.wade.cooney@gmail.com>
2018-10-10 15:29:47 -0600

    Scritping Classwork
    Scritping Classwork

---
+++ Class Project/Assets/LocalVars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalVars : MonoBehaviour
+{
+    public string Name;
+
+    public void Call()
+    {
+        Name = "Bob";
+    }
+
+    public void Call(String newName)
+    {
+        print(newName);
+        print(Name);
+    }
+
+}
---
+++ Class Project/Assets/WhatCanSeeIt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCanSeeIt: ScriptableObject
+{
+	private float health;
+	protected float PowerLevel;
+	public float EnergyLevel;
+
+	private void AddHealth()
+	{
+		health += 100;
+	}
+
+	public void AddPower
+	{
+		AddHealth();
+	}
+
+	{
+}
+
+public class ICanSeeIt : MonoBehaviour
+{
+	
+	}
+
+		
+	}
+}
