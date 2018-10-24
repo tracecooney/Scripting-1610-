c84aa4099e4436f26c2b00867f72f7b1f35ba990
trace cooney <trace.wade.cooney@gmail.com>
2018-10-24 15:12:46 -0600

    homework for class

--- Class Project pt2/loops Class Project/Assets/Loops.cs
+++ Class Project pt2/loops Class Project/Assets/Loops.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 public class Loops : MonoBehaviour
 {
 	public string[] Names;
+	public GameObject[] Things;
 
 	// Use this for initialization
 	void Start () {
+
+		for (int i = 0; i < Names.Length; i++)
+			{
+				print(Names[i]);
+			}
+
+		foreach (var thing in Things)
+			{
+				print(thing);
+			}
 		
-		for (int i = 0; i < 50; i++)
-		{
-			print("Word " + i);
-		}
 	}
 	
 	// Update is called once per frame
